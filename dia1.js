0882873f56e6caa3a58e945347bb0fb4ecb8cfc5
Zanqueta <matheuszanqueta20@gmail.com>
2025-09-11 16:42:03 -0300

    finalizando o dia 1

--- dia1.js
+++ dia1.js
@@ -10,19 +10,19 @@ let stringTrinta = '30';
 let numeroDez = 10;
 let stringDez = '10';
 
-if (numeroUm == stringUm) {
+if (numeroUm === stringUm) {
   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
 } else {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
 
-if (numeroTrinta == stringTrinta) {
+if (numeroTrinta === stringTrinta) {
   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
 } else {
   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
 }
 
-if (numeroDez == stringDez) {
+if (numeroDez === stringDez) {
   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
 } else {
   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
@@ -36,19 +36,19 @@ stringTrinta = 30
 numeroDez = 10
 stringDez = 10
 
-if ( numeroUm == stringUm) {
+if ( numeroUm === stringUm) {
   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
 } else {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
 
-if (numeroTrinta == stringTrinta) {
+if (numeroTrinta === stringTrinta) {
   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
 } else {
   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
 }
 
-if (numeroDez == stringDez) {
+if (numeroDez === stringDez) {
   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
 } else {
   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
